bc1ffb93a64bdec0acbbbeb3ff0acc70326c68d3
siri <ksiri1707@gmail.com>
2026-07-04 10:25:40 +0530

    webserver set-up

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,38 @@
-console.log("hello from index.js");
-alert("hello from index.js");
+const users = [
+  {
+    name: "Jane Doe",
+    gender: "Female",
+    image: "./images/jane.png"
+  },
+  {
+    name: "John Doe",
+    gender: "Male",
+    image: "./images/john.png"
+  }
+];
+
+let curUserId = 0;
+
+function toggle() {
+  curUserId = curUserId === 0 ? 1 : 0;
+
+  document.getElementById("img").src = users[curUserId].image;
+  document.getElementById("card-name").innerText = users[curUserId].name;
+  document.getElementById("card-gender").innerText = users[curUserId].gender;
+}
+
+function getRandomUser() {
+  fetch("https://randomuser.me/api/")
+    .then((response) => response.json())
+    .then((data) => {
+      const user = data.results[0];
+
+      document.getElementById("img").src = user.picture.large;
+      document.getElementById("card-name").innerText =
+        `${user.name.first} ${user.name.last}`;
+      document.getElementById("card-gender").innerText = user.gender;
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
+}
